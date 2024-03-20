3595c68a54869127e28b9f0c997529454d6f1000
ksj970714 <ksj970714@gmail.com>
2024-03-20 11:11:08 +0900

    second commit: Landing Page 내용 수정

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React: 수정 사항 Commit 
         </a>
       </header>
     </div>
